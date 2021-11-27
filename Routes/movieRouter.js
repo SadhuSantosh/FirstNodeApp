23886aea2b12663649d81e108f9918d9af87943c
santoshsadhu <santoshsadhu@gmail.com>
2021-11-27 16:37:08 +0530

    Refactored routes

--- Routes/movieRouter.js
+++ Routes/movieRouter.js
@@ -3,56 +3,46 @@ import { getMovies, getMovieById, deleteMovieById, addMovie, updateMovieById } f
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.route("/")
+.get(async (req, res) => {
     const filter = req.query;
     console.log(filter);
     if(filter.rating)
     {
         filter.rating= +filter.rating ;
     }
-    
     let moviesAll = await getMovies(filter);
-
     console.log(moviesAll);
     res.send(moviesAll);
-   
 })
-router.get("/:id",async (req, res) => {
+.post(async (req, res) => {    
+    const data=req.body;
+    let movies = await addMovie(data);
+    res.send(movies);
+})
+
+router.route("/:id")
+.get(async (req, res) => {
     const { id } = req.params;
     console.log("Movie ID is", id);
-
-    const client=await createConnection();
-
     let movie = await getMovieById(id);
     const notfound = {
         message: `The movie ID with ${id} doesn't exist !!`,
     }
     movie ? res.send(movie) : res.status(404).send(notfound);
 })
-router.delete("/:id",async (req, res) => {
+.delete(async (req, res) => {
     const { id } = req.params;
-    const client=await createConnection();
-
     let movie = await deleteMovieById(id);
-    
     const notfound = {
         message: `The movie ID with ${id} doesn't exist !!`,
     }
     movie ? res.send(movie) : res.status(404).send(notfound);
 })
-router.post("/", async (req, res) => {    
-    const client=await createConnection();
-    const data=req.body;
-    let movies = await addMovie(data);
-    
-    res.send(movies);
-})
-router.put("/:id", async (req, res) => {  
+.put(async (req, res) => {  
     const { id } = req.params;  
-    const client=await createConnection();
     const data=req.body;
     let movies = await updateMovieById(id, data);
-    
     res.send(movies);
 })
 
